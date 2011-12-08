ff37f3105c889e58165a6c2ffb245baceca01952
Rob Smith <kormoc@gmail.com>
2011-12-08 00:57:39 -0800

    Change html title

--- templates/header.php
+++ templates/header.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>ptqdui</title>
+        <title>Query Digest UI</title>
         <link rel="stylesheet" href="css/cupertino/jquery-ui-1.8.16.custom.css" type="text/css" media="all" >
         <link rel="stylesheet" href="css/style.css" type="text/css" media="all" >
 		<link rel="stylesheet" href="css/superfish.css" media="screen" >
-			
+
         <script src="js/jquery-1.6.2.min.js" type="text/javascript"></script>
         <script src="js/jquery-ui-1.8.16.custom.min.js" type="text/javascript"></script>
         <script src="js/jquery.dataTables.js" type="text/javascript"></script>
 		<script src="js/hoverIntent.js" type="text/javascript"></script>
 		<script src="js/superfish.js" type="text/javascript"></script>
 		<script src="js/supersubs.js" type="text/javascript"></script>
-		
-		<script> 
-		   $(document).ready(function() { 
-			   $('ul.sf-menu').superfish({ 
-				   delay:       1000,                            // one second delay on mouseout 
-				   animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation 
-				   speed:       'fast',                          // faster animation speed 
-				   autoArrows:  false,                           // disable generation of arrow mark-up 
-				   dropShadows: false                            // disable drop shadows 
-			   }); 
-		   }); 
+
+		<script>
+		   $(document).ready(function() {
+			   $('ul.sf-menu').superfish({
+				   delay:       1000,                            // one second delay on mouseout
+				   animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation
+				   speed:       'fast',                          // faster animation speed
+				   autoArrows:  false,                           // disable generation of arrow mark-up
+				   dropShadows: false                            // disable drop shadows
+			   });
+		   });
 	   </script>
-		
+
 	</head>
 	<body>
 
@@ -35,5 +35,5 @@
 			<li><a href="list-reviewed.php">Reviewed Queries</a></li>
 		</ul>
 	</div>
-	
+
 	<div id="content">
