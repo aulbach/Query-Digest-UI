c7927bf0a36f199a49b0186e8dc502d8ea0ed516
Rob Smith <kormoc@gmail.com>
2012-01-06 14:48:13 -0800

    UI overhaul again, working with jquery.dataTables.columnFilter for our filtering needs

--- list.php
+++ list.php
@@ -1,56 +1,56 @@
 
 <?php include('templates/header.php'); ?>
 
-
-<div class="tabs">
-    <ul>
-        <li><a href="#tabs-data">Data</a></li>
-		<li><a href="#tabs-filters">Filters</a></li>
-	</ul>
-    <div id="tabs-filters">
-        <p>Last Seen between  <input type="text" id="lastSeenStart" class="datepicker"> and <input type="text" id="lastSeenEnd" class="datepicker"></p>
-    </div>
-    <div id="tabs-data">
-        <table id="Queries">
-            <thead>
-                <tr>
-                    <th id="queriesColCount"      class="count"       > Count        </th>
-                    <th id="queriesColTime"       class="time"        > Total ms     </th>
-                    <th id="queriesColAvgTime"    class="avgTime"     > Avg ms       </th>
-                    <th id="queriesColFirstSeen"  class="firstSeen"   > First Seen   </th>
-                    <th id="queriesColLastSeen"   class="lastSeen"    > Last Seen    </th>
-                    <th id="queriesColSample"     class="sample"      > Sample Query </th>
-                    <th id="queriesColReviewedOn" class="reviewed_on" > Reviewed On  </th>
-                    <th id="queriesColReviewedBy" class="reviewed_by" > Reviewed By  </th>
-                    <th id="queriesColComments"   class="comments"    > Comments     </th>
-                    <th id="queriesColDetails"    class="details"     > &nbsp;       </th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    while ($row = $list->fetch(PDO::FETCH_ASSOC)) {
-                        echo "<tr>";
-                        echo "<td class='count number'>".$row['count']."</td>";
-                        echo "<td class='time number'>".$row['time']."</td>";
-                        echo "<td class='avgTime number'>".$row['time_avg']."</td>";
-                        echo "<td class='firstSeen date'>".$row['first_seen']."</td>";
-                        echo "<td class='lastSeen date'>".$row['last_seen']."</td>";
-                        echo "<td class='sample'>".$row['sample']."</td>";
-                        
-                        echo "<td class='reviewed_on'>".$row['reviewed_on']."</td>";
-                        echo "<td class='reviewed_by'>".$row['reviewed_by']."</td>";
-                        echo "<td class='comments'>".$row['comments']."</td>";
-                        
-                        echo '<td class="details"><a class="details" href="review.php?checksum='.$row['checksum'].'"><img src="images/details_open.png"></a></td>';
-                        echo "</tr>";
-                    }
-                ?>
-            </tbody>
-            <tfoot>
-            </tfoot>
-        </table>
-    </div>
-</div>
+<table id="Queries">
+    <thead>
+        <tr>
+            <th id="queriesColCount"      class="count"       > Count        </th>
+            <th id="queriesColTime"       class="time"        > Total ms     </th>
+            <th id="queriesColAvgTime"    class="avgTime"     > Avg ms       </th>
+            <th id="queriesColFirstSeen"  class="firstSeen"   > First Seen   </th>
+            <th id="queriesColLastSeen"   class="lastSeen"    > Last Seen    </th>
+            <th id="queriesColSample"     class="sample"      > Sample Query </th>
+            <th id="queriesColReviewedOn" class="reviewed_on" > Reviewed On  </th>
+            <th id="queriesColReviewedBy" class="reviewed_by" > Reviewed By  </th>
+            <th id="queriesColComments"   class="comments"    > Comments     </th>
+            <th id="queriesColDetails"    class="details"     > &nbsp;       </th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+            while ($row = $list->fetch(PDO::FETCH_ASSOC)) {
+                echo "<tr>";
+                echo "<td class='count number'>".$row['count']."</td>";
+                echo "<td class='time number'>".$row['time']."</td>";
+                echo "<td class='avgTime number'>".$row['time_avg']."</td>";
+                echo "<td class='firstSeen date'>".$row['first_seen']."</td>";
+                echo "<td class='lastSeen date'>".$row['last_seen']."</td>";
+                echo "<td class='sample'>".$row['sample']."</td>";
+                
+                echo "<td class='reviewed_on'>".$row['reviewed_on']."</td>";
+                echo "<td class='reviewed_by'>".$row['reviewed_by']."</td>";
+                echo "<td class='comments'>".$row['comments']."</td>";
+                
+                echo '<td class="details"><a class="details" href="review.php?checksum='.$row['checksum'].'"><img src="images/details_open.png"></a></td>';
+                echo "</tr>";
+            }
+        ?>
+    </tbody>
+    <tfoot>
+        <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+    </tfoot>
+</table>
 
 <script type="text/javascript">
 $(function() {
@@ -60,37 +60,35 @@ $(function() {
     $('#Queries').dataTable({
         "sDom":             '"R<"H"Cfr>t<"F"ilp>"',
         "bJQueryUI":        true,
-        "bStateSave":       true,
+        "bStateSave":       false,
         "bProcessing":      true,
         "aaSort":           [],
         "aoColumnDefs": [
                 { "bSearchable": false, "bVisible": true,  "aTargets": [ 0 ] },
-                { "bSearchable": false, "bVisible": false, "aTargets": [ 1 ] },
+                { "bSearchable": false, "bVisible": true, "aTargets": [ 1 ] },
                 { "bSearchable": false, "bVisible": true,  "aTargets": [ 2 ] },
-                { "bSearchable": false, "bVisible": false, "aTargets": [ 3 ] },
+                { "bSearchable": false, "bVisible": true, "aTargets": [ 3 ] },
                 { "bSearchable": false, "bVisible": true,  "aTargets": [ 4 ] },
                 { "bSearchable": true,  "bVisible": true,  "aTargets": [ 5 ] },
-                { "bSearchable": false, "bVisible": false, "aTargets": [ 6 ] },
-                { "bSearchable": true,  "bVisible": false, "aTargets": [ 7 ] },
-                { "bSearchable": true,  "bVisible": false, "aTargets": [ 8 ] },
+                { "bSearchable": false, "bVisible": true, "aTargets": [ 6 ] },
+                { "bSearchable": true,  "bVisible": true, "aTargets": [ 7 ] },
+                { "bSearchable": true,  "bVisible": true, "aTargets": [ 8 ] },
                 { "bSearchable": false, "bVisible": true,  "aTargets": [ 9 ], "bSortable": false },
             ]
-    });
-
-    var lastSeenDates = $( "#lastSeenStart, #lastSeenEnd" ).datepicker({
-        defaultDate: "+0d",
-        maxDate: '+0d',
-        changeMonth: true,
-        numberOfMonths: 1,
-        onSelect: function( selectedDate ) {
-            var option = this.id == "lastSeenStart" ? "minDate" : "maxDate",
-                instance = $( this ).data( "datepicker" ),
-                date = $.datepicker.parseDate(
-                    instance.settings.dateFormat ||
-                    $.datepicker._defaults.dateFormat,
-                    selectedDate, instance.settings );
-            lastSeenDates.not( this ).datepicker( "option", option, date );
-        }
+    }).columnFilter({
+        sPlaceHolder: 'tfoot',
+        aoColumns: [
+            { type: "number-range" },
+            { type: "number-range" },
+            { type: "number-range" },
+            { type: "date-range" },
+            { type: "date-range" },
+            { type: "text" },
+            { type: "date-range" },
+            { type: "select" },
+            { type: "text" },
+            null
+        ]
     });
 });
 
--- templates/header.php
+++ templates/header.php
@@ -2,43 +2,29 @@
 <html>
     <head>
         <title>Query Digest UI</title>
-        <link rel="stylesheet" href="css/cupertino/jquery-ui-1.8.16.custom.css" type="text/css" media="all" >
-        <link rel="stylesheet" href="css/style.css" type="text/css" media="all" >
-		<link rel="stylesheet" href="css/superfish.css" media="screen" >
-		<link rel="stylesheet" href="js/fancybox/jquery.fancybox-1.3.4.css" media="screen" >
-		<link rel="stylesheet" href="css/ColVis.css" media="screen" >
-		<link rel="stylesheet" href="css/ColReorder.css" media="screen" >
-
-        <script src="js/jquery-1.6.2.min.js" type="text/javascript"></script>
-        <script src="js/jquery-ui-1.8.16.custom.min.js" type="text/javascript"></script>
-        <script src="js/jquery.dataTables.js" type="text/javascript"></script>
-		<script src="js/hoverIntent.js" type="text/javascript"></script>
-		<script src="js/superfish.js" type="text/javascript"></script>
-		<script src="js/supersubs.js" type="text/javascript"></script>
+        
+		<script src="js/jquery-1.6.2.min.js" type="text/javascript"></script>
 		
+        <script src="js/jquery-ui-1.8.16.custom.min.js" type="text/javascript"></script>
+		<link rel="stylesheet" href="css/cupertino/jquery-ui-1.8.16.custom.css" type="text/css" media="all" >
+			
+		<script src="js/DataTables/media/js/jquery.dataTables.js" type="text/javascript"></script>
+		<script src="js/DataTables/extras/jquery-datatables-column-filter/media/js/jquery.dataTables.columnFilter.js" type="text/javascript"></script>
+		
+		<script src="js/DataTables/extras/ColVis-1.0.6/media/js/ColVis.min.js" type="text/javascript"></script>
+		<link rel="stylesheet" href="js/DataTables/extras/ColVis-1.0.6/media/css/ColVis.css" media="screen" >
+			
+		<script src="js/DataTables/extras/ColReorder-1.0.4/media/js/ColReorder.min.js" type="text/javascript"></script>
+		<link rel="stylesheet" href="js/DataTables/extras/ColReorder-1.0.4/media/css/ColReorder.css" media="screen" >
+		
+		<link rel="stylesheet" href="js/fancybox/jquery.fancybox-1.3.4.css" media="screen" >
 		<script src="js/fancybox/jquery.fancybox-1.3.4.pack.js" type="text/javascript"></script>
 		<script src="js/fancybox/jquery.easing-1.3.pack.js" type="text/javascript"></script>
 		
-		<script src="js/ColVis.min.js" type="text/javascript"></script>
-		<script src="js/ColReorder.min.js" type="text/javascript"></script>
+		<link rel="stylesheet" href="css/style.css" type="text/css" media="all" >
 		
 		<script>
 		   $(document).ready(function() {
-            
-				if (top != self) {
-					$('#navMenu').replaceWith('');
-					$('#content').css('padding-top', '4px');
-					$('#content').css('padding', '4px');
-				}
-				else {
-					$('ul.sf-menu').superfish({
-						delay:       1000,                            // one second delay on mouseout
-						animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation
-						speed:       'fast',                          // faster animation speed
-						autoArrows:  false,                           // disable generation of arrow mark-up
-						dropShadows: false                            // disable drop shadows
-					});
-				}
                
 				$("a.details").fancybox({
 					type: 			'iframe',
@@ -54,27 +40,5 @@
 
 	</head>
 	<body>
-
-	<div id="navMenu">
-		<ul id="menu" class="sf-menu">
-			<li><a href="list-unreviewed.php" title="Never reviewed queries">Unreviewed Queries</a>
-                <ul>
-                    <li><a href="list-unreviewed.php">All</a></li>
-                    <li><a href="list-unreviewed-past-month.php">Past Month</a></li>
-                    <li><a href="list-unreviewed-past-two-week.php">Past Two Weeks</a></li>
-                    <li><a href="list-unreviewed-past-week.php">Past Week</a></li>
-                </ul>
-            </li>
-			<li><a href="list-reviewed-seen.php" title="Queries seen after a review">Seen after Reviewed Queries</a></li>
-			<li><a href="list-reviewed.php" title="Reviewed Queries">Reviewed Queries</a>
-                <ul>
-                    <li><a href="list-reviewed.php">All</a></li>
-                    <li><a href="list-reviewed-past-month.php">Past Month</a></li>
-                    <li><a href="list-reviewed-past-two-week.php">Past Two Weeks</a></li>
-                    <li><a href="list-reviewed-past-week.php">Past Week</a></li>
-                </ul>
-            </li>
-		</ul>
-	</div>
 
 	<div id="content">
