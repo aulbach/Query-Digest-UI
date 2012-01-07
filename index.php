161b58a7535b36b306f1a0b6ac138d579e077e2c
Rob Smith <kormoc@gmail.com>
2012-01-06 17:02:24 -0800

    Remove unneeded js

--- index.php
+++ index.php
@@ -178,28 +178,4 @@ $list->execute();
 		   </script>
 		</div>
     </body>
-
-<script type="text/javascript">
-    $(function() {
-        $( ".accordionOpen" ).accordion({
-            collapsible: 		true
-        });
-
-        $( ".accordion" ).accordion({
-            collapsible: 		true,
-            active: 			false
-        });
-		
-		$( ".datepicker" ).datepicker({
-			showOtherMonths: 	true ,
-			selectOtherMonths: 	true,
-			maxDate: 			'+0d',
-			duration: 			'fast'
-		});
-		
-		$( ".tabs" ).tabs({
-        });
-    });
-</script>
-
 </html>
