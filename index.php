dbbab0e355befdedac411686873696829005a38e
Rob Smith <kormoc@gmail.com>
2012-01-09 12:03:27 -0800

    Round total ms counts

--- index.php
+++ index.php
@@ -4,21 +4,21 @@
 	
 	$dbh = new PDO("mysql:host={$reviewhost['db_host']};dbname={$reviewhost['db_database']}", $reviewhost['db_user'], $reviewhost['db_password']);
 	
-	$list = $dbh->prepare('SELECT review.checksum                            AS checksum,
-								  SUBSTR(review.fingerprint, 1, 99999)       AS sample,
+	$list = $dbh->prepare('SELECT review.checksum                            				AS checksum,
+								  SUBSTR(review.fingerprint, 1, 99999)       				AS sample,
 								  
-								  DATE(review.first_seen)                    AS first_seen,
-								  DATE(review.last_seen)                     AS last_seen,
-								  IFNULL(review.reviewed_by, "-")      	     AS reviewed_by,
-								  DATE(review.reviewed_on)				     AS reviewed_on,
-								  review.comments						     AS comments,
+								  DATE(review.first_seen)                    				AS first_seen,
+								  DATE(review.last_seen)                     				AS last_seen,
+								  IFNULL(review.reviewed_by, "-")      	     				AS reviewed_by,
+								  DATE(review.reviewed_on)				     				AS reviewed_on,
+								  review.comments						     				AS comments,
 								  
-								  SUM(history.ts_cnt)                        AS count,
-								  SUM(history.query_time_sum)                AS time,
+								  SUM(history.ts_cnt)                        				AS count,
+								  ROUND(SUM(history.query_time_sum), 2)      				AS time,
 								  ROUND(SUM(history.query_time_sum)/SUM(history.ts_cnt), 2) AS time_avg
 								  
-							 FROM '.$reviewhost['review_table'].'            AS review
-						LEFT JOIN '.$reviewhost['review_history_table'].'    AS history
+							 FROM '.$reviewhost['review_table'].'            				AS review
+						LEFT JOIN '.$reviewhost['review_history_table'].'    				AS history
 							   ON history.checksum = review.checksum
 						 GROUP BY review.checksum
 							   ');
