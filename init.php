a1f9bfb67cbaa4426d49c50decc9198bf192d660
Rob Smith <kormoc@gmail.com>
2012-01-20 16:12:28 -0800

    Add in a way to avoid scanning for databases on remote hosts

--- init.php
+++ init.php
@@ -7,13 +7,15 @@
 	unset($query);
 
 	foreach ($explainhosts as $label => $host) {
-		$ebh = new PDO($host['dsn'], $host['user'], $host['password']);
-		$query = $ebh->prepare('SHOW DATABASES');
-		$query->execute();
-		while (list($database) = $query->fetch(PDO::FETCH_NUM))
-			$explainhosts[$label]['databases'][] = $database;
-		$query->closeCursor();
-		unset($ebh);
+		if (!key_exists('databases', $explainhosts[$label])) {
+			$ebh = new PDO($host['dsn'], $host['user'], $host['password']);
+			$query = $ebh->prepare('SHOW DATABASES');
+			$query->execute();
+			while (list($database) = $query->fetch(PDO::FETCH_NUM))
+				$explainhosts[$label]['databases'][] = $database;
+			$query->closeCursor();
+			unset($ebh);
+		}
 	}
 
     require('libs/sqlquery/SqlParser.php');
