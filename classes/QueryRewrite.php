c61536ae2d9e8077ae7f478dd7190cfdf33f069c
Rob Smith <kormoc@gmail.com>
2012-02-28 10:13:51 -0800

    Move some props back to private

--- classes/QueryRewrite.php
+++ classes/QueryRewrite.php
@@ -1,8 +1,8 @@
 <?php
 
     class QueryRewrite{
-		public $sql 	  = null;
-		public $type 	  = 0;
+		private $sql 	  = null;
+		private $type 	  = 0;
 		
 		const UNKNOWN     = 0;
 		const SELECT      = 1;
